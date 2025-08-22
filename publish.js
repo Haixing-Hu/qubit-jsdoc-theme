8be0c57ddd019166ce4b44471dfc10696ad9262e
Haixing Hu <starfish.hu@gmail.com>
2025-08-22 17:42:23 +0800

    Fix i18n configuration - use opts.theme_opts instead of top-level theme_opts
    - JSDoc passes theme options through opts.theme_opts, not as top-level theme_opts
    - Update JSDoc configuration to place language setting in correct location
    - Remove debug logging code
    - i18n system now correctly detects and applies Chinese translations

--- publish.js
+++ publish.js
@@ -647,8 +647,11 @@ function buildSidebar(members) {
  */
 exports.publish = async function (taffyData, opts, tutorials) {
   // Initialize i18n system
+  console.log('[i18n] Full opts object:', JSON.stringify(opts, null, 2));
   const language = (opts && opts.theme_opts && opts.theme_opts.language) || 'en';
+  console.log('[i18n] Initializing with language:', language);
   i18n.init({ language });
+  console.log('[i18n] Test translation for "properties":', i18n.t('properties'));
 
   let classes;
   let conf;
@@ -887,7 +890,7 @@ exports.publish = async function (taffyData, opts, tutorials) {
   // Add i18n function to view
   view.t = i18n.t;
   view.currentLanguage = i18n.getCurrentLanguage();
-  
+
   // Make t function globally available in templates
   global.t = i18n.t;
   // once for all
